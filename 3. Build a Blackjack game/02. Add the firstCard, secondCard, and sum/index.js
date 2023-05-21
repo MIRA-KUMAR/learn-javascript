560e78eb61e5cbf88123cae6cbff6e35d46ee244
MIRA-KUMAR <mira.kumar.24@gmail.com>
2023-05-20 21:21:30 -0700

    Building muscle memory

--- 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
@@ -2,3 +2,18 @@
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = Math.floor(Math.random() * 12) + 1;
+let secondCard = Math.floor(Math.random() * 12) + 1;
+
+let sum = firstCard + secondCard;
+
+if (sum < 21) {
+    console.log("Want to draw more card?");
+}
+else if (sum === 21) {
+    console.log("You won!!");
+}
+else {
+    console.log("You lost!");
+}
